963e18730530c5beb4b521c751266c0a0c0de518
Alexei Griscenco <alexeygr3000@mail.ru>
2022-06-15 18:29:50 +0300

    Flexible HTML content (functions)

--- hello.hs
+++ hello.hs
@@ -1,4 +1,15 @@
 main = putStrLn myhtml
 
+html_ content = "<html>" <> content <> "</html>"
 
-myhtml = "<html><body>Hello, world!</body></html>"
+body_ content = "<body>" <> content <> "</body>"
+
+wrapHtml = html_ . body_
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
+
+makeHtml title content = html_ (head_ $ title_ title <> body_ content)
+
+myhtml = makeHtml "Hello, title!" "Hello, world!"
